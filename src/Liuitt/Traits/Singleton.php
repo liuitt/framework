b539e271e593e0bc851378404f5fb0053bd5e84e
liuitt <liuitt@liuitt.com>
2015-05-11 21:12:44 -0300

    Create Singleton.php

---
+++ src/Liuitt/Traits/Singleton.php
@@ -0,0 +1,29 @@
+<?php
+namespace Liuitt\Traits;
+
+use ReflectionClass;
+
+trait Singleton
+{
+
+    protected static $instance;
+
+    final public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            $class = new ReflectionClass(__CLASS__);
+            self::$instance = $class->newInstanceArgs(func_get_args());
+        }
+
+        return self::$instance;
+    }
+
+    final private function __clone()
+    {
+    }
+
+    final private function __wakeup()
+    {
+    }
+
+}
